0dabd30a86710202240ed3a65f18bd63a40a5afe
OskarsZageris <oskars.zageris@gmail.com>
2022-04-27 15:19:58 +0300

    tests for Qix

--- tests/FooBarTest.php
+++ tests/FooBarTest.php
@@ -41,4 +41,44 @@ class FooBarTest extends TestCase
         $this->assertSame('22', $number->checkNumber(22));
         $this->assertSame('667', $number->checkNumber(667));
     }
+
+    public function testCheckNumberShouldReturnQix()
+    {
+        $number = new FooBar();
+        $this->assertSame('Qix', $number->checkNumber(7));
+        $this->assertSame('Qix', $number->checkNumber(14));
+        $this->assertSame('Qix', $number->checkNumber(28));
+        $this->assertSame('Qix', $number->checkNumber(49));
+    }
+
+
+    public function testCheckNumberShouldReturnFooQix()
+    {
+        $number = new FooBar();
+        $this->assertSame('FooQix', $number->checkNumber(21));
+        $this->assertSame('FooQix', $number->checkNumber(42));
+        $this->assertSame('FooQix', $number->checkNumber(63));
+        $this->assertSame('FooQix', $number->checkNumber(126));
+    }
+
+    public function testCheckNumberShouldReturnBarQix()
+    {
+        $number = new FooBar();
+        $this->assertSame('BarQix', $number->checkNumber(35));
+        $this->assertSame('BarQix', $number->checkNumber(70));
+        $this->assertSame('BarQix', $number->checkNumber(140));
+        $this->assertSame('BarQix', $number->checkNumber(280));
+    }
+    public function testCheckNumberShouldReturnFooBarQix()
+    {
+        $number = new FooBar();
+        $this->assertSame('FooBarQix', $number->checkNumber(105));
+        $this->assertSame('FooBarQix', $number->checkNumber(210));
+        $this->assertSame('FooBarQix', $number->checkNumber(525));
+        $this->assertSame('FooBarQix', $number->checkNumber(735));
+    }
+
+
+
+
 }
